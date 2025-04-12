60de5819d2c437c469b5fc10f01aefbf5c76317e
Aleksandr Meertsuk <aleksandrme3@gmail.com>
2025-04-12 17:57:36 +0400

    решил две задачи и сделал карусель

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,35 @@
 function initCarousel() {
-  // ваш код...
+  const carouselInner = document.querySelector('.carousel__inner');
+  const arrowLeft = document.querySelector('.carousel__arrow_left');
+  const arrowRight = document.querySelector('.carousel__arrow_right');
+
+  const slideCount = document.querySelectorAll('.carousel__slide').length;
+  const slideWidth = carouselInner.offsetWidth;
+
+  let currentSlide = 0;
+
+  arrowLeft.style.display = 'none';
+
+  arrowRight.addEventListener('click', () => {
+    currentSlide += 1;
+
+    carouselInner.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+    arrowLeft.style.display = '';
+
+    if (currentSlide === slideCount - 1) {
+      arrowRight.style.display = 'none';
+    }
+  });
+
+
+  arrowLeft.addEventListener('click', () => {
+    currentSlide -= 1;
+
+    carouselInner.style.transform = `translateX(-${slideWidth * currentSlide}px)`;
+    arrowRight.style.display = '';
+
+    if (currentSlide === 0) {
+      arrowLeft.style.display = 'none';
+    }
+  });
 }
