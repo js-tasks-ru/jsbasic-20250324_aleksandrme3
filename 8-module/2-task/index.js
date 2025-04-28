ae4ff11ccc700e2ea655b9d54ab1a96e153a7ada
Aleksandr Meertsuk <aleksandrme3@gmail.com>
2025-04-28 20:21:58 +0400

    сделал иконку корзины, список товаров и корзину

--- 8-module/2-task/index.js
+++ 8-module/2-task/index.js
@@ -5,5 +5,46 @@ export default class ProductGrid {
   constructor(products) {
     this.products = products;
     this.filters = {};
+
+    this.render();
+    this.renderProducts();
   }
+
+  render() {
+    this.elem = createElement(`
+      <div class="products-grid">
+        <div class="products-grid__inner"></div>
+      </div>
+      `);
+
+    this.inner = this.elem.querySelector('.products-grid__inner');
+  }
+
+
+  renderProducts() {
+    this.inner.innerHTML = '';
+
+    let filteredProducts = this.products.filter(product => {
+
+      let conditions = [
+        !(this.filters.noNuts && product.nuts),
+        !(this.filters.vegeterianOnly && !product.vegeterian),
+        !(this.filters.maxSpiciness !== undefined && product.spiciness > this.filters.maxSpiciness),
+        !(this.filters.category && product.category !== this.filters.category)
+      ];
+
+      return conditions.every(condition => condition);
+    });
+
+    for (let product of filteredProducts) {
+      let card = new ProductCard(product);
+      this.inner.append(card.elem);
+    }
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.renderProducts();
+  }
+
 }
